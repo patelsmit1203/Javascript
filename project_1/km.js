cb8e292d609a49420c9b4da6d87efc34c570d3d5
patelsmit1203 <smitpatel8262@gmail.com>
2025-10-09 18:50:28 +0530

    Add files via upload

---
+++ project_1/km.js
@@ -0,0 +1,11 @@
+  // km to meter
+
+        let km=Number(prompt("enter your km "))
+        let meter=(km*1000)
+        console.log(meter)
+
+ // meter to km
+
+        let mtr=Number(prompt("enter your mtr"))
+        let km=(mtr/1000)
+        console.log(km)
---
+++ project_1/odd_even.js
@@ -0,0 +1,10 @@
+let x=Number(prompt("enter number"))
+
+if(x%2===0){
+    console.log("this number is even");
+    
+}
+else{
+    console.log("this number is odd");
+    
+}
