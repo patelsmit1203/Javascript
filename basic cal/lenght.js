04760510b61a0b3ed56e4f298c04e3f3ff54a090
patelsmit1203 <smitpatel8262@gmail.com>
2025-11-24 16:49:12 +0530

    Add files via upload

---
+++ basic cal/lenght.js
@@ -0,0 +1,7 @@
+const arr=[10,20,30,40,50]
+let count=0
+
+for(let i in arr){
+    count++
+}
+console.log(count);
---
+++ basic cal/value_array.js
@@ -0,0 +1,4 @@
+const arr=[1,2,2,3,4,4,5]
+const unique=[...new Set(arr)]
+
+console.log(unique);
